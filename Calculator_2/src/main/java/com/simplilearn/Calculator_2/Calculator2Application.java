fc00cec01a69f1ad1aefa601cf3e24d87b1a7a0e
Jenkins Automation Server <jenkins@node1.example.com>
2021-11-05 00:27:38 -0700

    Calculator 2 commit

---
+++ Calculator_2/src/main/java/com/simplilearn/Calculator_2/Calculator2Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Calculator_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator2Application.class, args);
+	}
+
+}
